d92ad92ca5dfdf0fb4880ff8ca58382262b36b5b
JacobTheRanga <jacobirving.nz@gmail.com>
2022-08-19 11:15:56 +1200

    completed gyro data processing

--- mpu6050.ino
+++ mpu6050.ino
@@ -1,48 +1,61 @@
 #include<Wire.h>
  
 const int MPU_addr=0x68;
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
  
 int minVal=265;
 int maxVal=402;
+
+int16_t accel[3];
+
+int gyro[3];
  
+double mappedGyro[3];
+
 double x;
 double y;
 double z;
  
 void setup(){
-Wire.begin();
-Wire.beginTransmission(MPU_addr);
-Wire.write(0x6B);
-Wire.write(0);
-Wire.endTransmission(true);
-Serial.begin(9600);
+    Wire.begin();
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x6B);
+    Wire.write(0);
+    Wire.endTransmission(true);
+    Serial.begin(9600);
 }
 void loop(){
-  delay(200);
-Wire.beginTransmission(MPU_addr);
-Wire.write(0x3B);
-Wire.endTransmission(false);
-Wire.requestFrom(MPU_addr,14,true);
-AcX=Wire.read()<<8|Wire.read();
-AcY=Wire.read()<<8|Wire.read();
-AcZ=Wire.read()<<8|Wire.read();
-int xAng = map(AcX,minVal,maxVal,-90,90);
-int yAng = map(AcY,minVal,maxVal,-90,90);
-int zAng = map(AcZ,minVal,maxVal,-90,90);
- 
-x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
-y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
-z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
- 
-Serial.print("AngleX= ");
-Serial.println(x);
- 
-Serial.print("AngleY= ");
-Serial.println(y);
- 
-Serial.print("AngleZ= ");
-Serial.println(z);
-Serial.println("-----------------------------------------");
-delay(400);
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_addr,14,true);
+    for (int i = 0; i < 3; i++){
+        accel[i] = Wire.read()<<8|Wire.read();
+    }
+    for (int i = 0; i < 3; i++){
+        gyro[i] = map(accel[i],minVal,maxVal,-90,90);
+    }
+    mappedGyro[0] = RAD_TO_DEG * (atan2(-gyro[1], -gyro[2])+PI);
+    mappedGyro[1] = RAD_TO_DEG * (atan2(-gyro[0], -gyro[2])+PI);
+    mappedGyro[2] = RAD_TO_DEG * (atan2(-gyro[0], -gyro[1])+PI);
+    
+    for (int i = 0; i < 3; i++){
+        if (mappedGyro[i] > 180){
+            mappedGyro[i] = mappedGyro[i] - 360;
+        }
+    }
+
+    x = mappedGyro[0];
+    y = mappedGyro[1];
+    z = mappedGyro[2];
+
+    Serial.print("AngleX= ");
+    Serial.println(x);
+    
+    Serial.print("AngleY= ");
+    Serial.println(y);
+    
+    Serial.print("AngleZ= ");
+    Serial.println(z);
+    Serial.println("-----------------------------------------");
+    delay(200);
 }
