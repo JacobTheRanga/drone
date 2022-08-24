4627a28929669c5c7dd720f25a4bbe196e727fba
JacobTheRanga <jacobirving.nz@gmail.com>
2022-08-24 15:04:14 +1200

    propotional motor control :)

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from passlib.hash import pbkdf2_sha256
 from flask import Flask, render_template, request, session, redirect, url_for
+from serial import Serial
 app = Flask(__name__)
 hashedpass = '$pbkdf2-sha256$29000$GqP0HkMIIQRgDKHU2jsHgA$48Cmw2qfq2bsOehmXkWXWy1Ccx./V9dWbJmtUxOXAFg'
 app.secret_key = os.urandom(64)
 
-import drone
+port = Serial('COM5')
 
 @app.route('/login', methods = ['get', 'post'])
 def login():
@@ -21,9 +22,15 @@ def login():
 def home():
     if not session or session['user'] != 'admin':
         return redirect(url_for('login'))
-    if request.method != 'POST':
-        return render_template('/home.html')
-    drone.blink()
+    while 1:
+        angle = {}
+        rawAngle = port.readline().decode('utf-8')
+        print(rawAngle)
+        angle['x'] = rawAngle.split()[1]
+        angle['y'] = rawAngle.split()[2]
+        angle['z'] = rawAngle.split()[3]
+        if request.method != 'POST':
+            return render_template('/home.html', angle = angle)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=80)
--- mpu6050.ino
+++ mpu6050.ino
@@ -1,9 +1,21 @@
 #include<Wire.h>
+#include<Servo.h>
+
+Servo motor1;
+Servo motor2;
  
 const int MPU_addr=0x68;
  
 int minVal=265;
 int maxVal=402;
+
+int maxpwm = 30;
+int maxangle = 10;
+int change = 1;
+int rate = 20;
+
+int motor1pwm = 0;
+int motor2pwm = 0;
 
 short accel[3];
 
@@ -22,6 +34,8 @@ void setup(){
     Wire.write(0);
     Wire.endTransmission(true);
     Serial.begin(9600);
+    motor1.attach(9, 1000, 2000);
+    motor2.attach(10, 1000, 2000);
 }
 void loop(){
     Wire.beginTransmission(MPU_addr);
@@ -48,10 +62,26 @@ void loop(){
     y = mappedGyro[1];
     z = mappedGyro[2];
 
-    Serial.print(x);
+    if (x > maxangle){
+        if (motor1pwm < maxpwm){
+        motor1pwm = motor1pwm +change;
+        }
+        if (motor2pwm > 0){
+        motor2pwm = motor2pwm -change;
+        }
+    }
+    if (x < -maxangle){
+        if (motor2pwm < maxpwm){
+        motor2pwm = motor2pwm +change;
+        }
+        if (motor1pwm > 0){
+        motor1pwm = motor1pwm -change;
+        }
+    }
+    motor1.write(motor1pwm);
+    motor2.write(motor2pwm);
+    Serial.print(motor1pwm);
     Serial.print(' ');
-    Serial.print(y);
-    Serial.print(' ');
-    Serial.println(z);
-    delay(10);
+    Serial.println(motor2pwm);
+    delay(1000/rate);
 }
